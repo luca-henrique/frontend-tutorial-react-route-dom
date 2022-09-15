bb2697cf8fd025ea75c5eee9dd3f0709ad7360dd
Lucas Henrique <lukas.paes18@gmail.com>
2022-09-14 23:39:07 -0300

    feature: criando primeiras rotas

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,3 +1,3 @@
 export const Home = () => {
-  return <div>Oi</div>;
+  return <div>Home</div>;
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client';
-import {Home} from './components/Home';
+import Routes from './routers/router';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <Home />,
+  <Routes />,
 );
