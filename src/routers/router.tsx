796c60ee921edab2ed66a03ee1309c6c3ef503e7
Lucas Henrique <lukas.paes18@gmail.com>
2022-09-14 23:43:41 -0300

    feature: link vs a

--- src/routers/router.tsx
+++ src/routers/router.tsx
@@ -2,10 +2,12 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 import {Home} from '../components/Home';
 import {About} from '../components/About';
+import {Menu} from '../components/Menu';
 
 const App = () => {
   return (
     <BrowserRouter>
+      <Menu />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
